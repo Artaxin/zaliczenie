e683d422e1a1e9cfeb722bf968af58a8ea928ebb
Artaxin <jakub.domoslawski@gmail.com>
2021-05-22 13:19:55 +0200

    ss

--- src/pl/wsb/Car.java
+++ src/pl/wsb/Car.java
@@ -3,7 +3,7 @@ package pl.wsb;
 public class Car extends Vehicle implements Activities{
 
     public Car(String plates, Long vin, String color, Double price, Integer fuelCons, Integer tank, Long counter) {
-
+    super(plates, vin, color, price, fuelCons, tank, counter);
     }
 
     @Override
